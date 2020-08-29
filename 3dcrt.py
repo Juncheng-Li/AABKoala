b8602cf4a09c77d1cbbd4b397936336d9cd3a121
Juncheng-Li <juncheng.alex.li@gmail.com>
2020-08-29 17:44:32 +0800

    axis fixed

--- 3dcrt.py
+++ 3dcrt.py
@@ -6,9 +6,28 @@ import numpy as np
 df = pd.read_excel("./Book1.xlsx")
 print(df)
 
-print(np.unique(df["x-axis"]))
+# print(df.groupby(["x-axis"]).count())
+# axis = df["x-axis"].unique()
+
+
+# y
+del df["x-axis"]
+# x
+x = pd.DataFrame({"x": 263 * list(range(1, 39))})
+# xy
+df["x"] = x
+print(df)
+
+xlabels = np.unique(df["x"])
 
 # plot.scatter(x, y, c="color", s=size)
-plt.scatter(df["x-axis"], df["a"])
-plt.xticks()
+plt.scatter(df["x"], df["a"])
+plt.xticks(xlabels, ('6', '10', '15', '18', '6FFF', "10FFF",
+                     "6", "10", "15", '18', '6FFF', '10FFF',
+                     '6', '6', "6", "6", "6", "6",
+                     '6', '10', '15', '18', '6FFF', "10FFF",
+                     "6", "10", "15", '18', '6FFF', '10FFF',
+                     '6', '10', "15", "18",
+                     "6", "10", "15", "18"), rotation=90)
+plt.ylim(-0.1, 0.1)
 plt.show()
