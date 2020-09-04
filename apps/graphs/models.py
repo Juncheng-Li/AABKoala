14fbac082733c9c419ac1ab17ebc0956a91a5b4b
YunningGong <81874262@qq.com>
2020-09-05 00:19:19 +0800

    add model Result and migrate to MySQL

--- apps/graphs/models.py
+++ apps/graphs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Result(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    AuditID = models.CharField(max_length=45)
+    RevisionNumber = models.CharField(max_length=45)
+    FacilityName = models.CharField(max_length=100)
+    FacilityID = models.CharField(max_length=45)
+    Auditor1 = models.CharField(max_length=45)
+    Auditor2 = models.CharField(max_length=45)
+    Auditor3 = models.CharField(max_length=45)
+    AuditDate = models.DateTimeField()
+    RepDate = models.CharField(max_length=45)
+    LinacModel = models.CharField(max_length=45)
+    LinacManufacturer = models.CharField(max_length=45)
+    PlanningSystemManufacturer = models.CharField(max_length=45)
+    tps = models.CharField(max_length=45)
+    Algorithm = models.CharField(max_length=45)
+    kqFac = models.CharField(max_length=10)
+    ACDS = models.CharField(max_length=10)
+    Phantom = models.CharField(max_length=45)
+
+    class Meta:
+        ordering = ['created']
