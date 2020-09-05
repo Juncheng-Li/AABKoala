87e495ba4345aefc274852c9b861935e8211d4c8
Juncheng-Li <juncheng.alex.li@gmail.com>
2020-09-05 21:08:09 +0800

    label adjusted

--- plot3DCRT.py
+++ plot3DCRT.py
@@ -17,7 +17,7 @@ def all_data():
 
     # y
     del df["x-axis"]
-    # x
+    # x-axis
     # x = pd.DataFrame({"x": 263 * (1, 2, 3, 4, 5, 6,
     #                               7, 8, 9, 10, 11, 12,
     #                               14, 16.5, 19, 21.5, 24, 26.5,
@@ -26,15 +26,17 @@ def all_data():
     #                               46, 47, 48, 49,
     #                               53, 54, 55, 56)})
     x = pd.DataFrame({"x": 263 * list(range(1, 39))})
-    # xy
     df["x"] = x
     print(df)
 
-    xlabels = np.unique(df["x"])
-
-    # plot.scatter(x, y, c="color", s=size)
+    # adjust canvas size
     plt.figure(figsize=(8.4, 4.8))
+
+    # scatter plot
     allData = plt.scatter(df["x"], df["a"], s=5, c="#424242", zorder=5)
+
+    # set axis
+    xlabels = np.unique(df["x"])
     plt.xticks(xlabels, ('6', '10', '15', '18', '6FFF', "10FFF",
                          "6", "10", "15", '18', '6FFF', '10FFF',
                          '6', '6', "6", "6", "6", "6",
@@ -45,13 +47,20 @@ def all_data():
     plt.yticks(fontsize=7)
     plt.ylim(-0.1, 0.1)
 
+    # add green background
     ax1 = plt.gca()
     ax1.axhspan(-0.03, 0.03, facecolor="#C5E1A5", alpha=1, zorder=2)
     ax1.axhspan(-0.05, 0.05, facecolor="#F1F8E9", alpha=1, zorder=1)
     # ax.set_facecolor("#99FF99")
 
-    plt.legend([allData], ['All Data'])
+    # set white margins
+    plt.subplots_adjust(right=0.83)
+
+    # set legend
+    plt.legend([allData], ['All Data'], bbox_to_anchor=(1.23, 1))
+
+    # save fig
     plt.savefig("3DCRT.png", dpi=300)
-    plt.show()
+    # plt.show()
 
 
