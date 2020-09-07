ac7c6dc9d08386dc3f3658d7b882864710b65d7f
YunningGong <81874262@qq.com>
2020-09-07 23:32:23 +0800

    add update nested serializers

--- apps/graphs/serializers.py
+++ apps/graphs/serializers.py
@@ -27,8 +27,43 @@ class ResultSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        facilityoutput_data = validated_data.pop('facilityOutput')
+        facility_outputs_data = validated_data.pop('facilityOutput')
         result = Result.objects.create(**validated_data)
-        for each in facilityoutput_data:
-            FacilityOutput.objects.create(result=result, **each)
+        for facility_output_data in facility_outputs_data:
+            FacilityOutput.objects.create(result=result, **facility_output_data)
         return result
+
+    def update(self, instance, validated_data):
+        facility_outputs_data = validated_data.pop('facilityOutput')
+        facilityOutputs = (instance.facilityOutput).all()
+        facilityOutputs = list(facilityOutputs)
+
+        instance.AuditID = validated_data.get('AuditID', instance.AuditID)
+        instance.RevisionNumber = validated_data.get('RevisionNumber', instance.RevisionNumber)
+        instance.FacilityName = validated_data.get('FacilityName', instance.FacilityName)
+        instance.FacilityID = validated_data.get('FacilityID', instance.FacilityID)
+        instance.Auditor1 = validated_data.get('Auditor1', instance.Auditor1)
+        instance.Auditor2 = validated_data.get('Auditor2', instance.Auditor2)
+        instance.Auditor3 = validated_data.get('Auditor3', instance.Auditor3)
+        instance.AuditDate = validated_data.get('AuditDate', instance.AuditDate)
+        instance.RepDate = validated_data.get('RepDate', instance.RepDate)
+        instance.LinacModel = validated_data.get('LinacModel', instance.LinacModel)
+        instance.LinacManufacturer = validated_data.get('LinacManufacturer', instance.LinacManufacturer)
+        instance.PlanningSystemManufacturer = validated_data.get('PlanningSystemManufacturer',
+                                                                 instance.PlanningSystemManufacturer)
+        instance.tps = validated_data.get('tps', instance.tps)
+        instance.Algorithm = validated_data.get('Algorithm', instance.Algorithm)
+        instance.kqFac = validated_data.get('kqFac', instance.kqFac)
+        instance.ACDS = validated_data.get('ACDS', instance.ACDS)
+        instance.Phantom = validated_data.get('Phantom', instance.Phantom)
+        instance.save()
+
+        for facility_output_data in facility_outputs_data:
+            facilityOutput = facilityOutputs.pop(0)
+            facilityOutput.energy_6 = facility_output_data.get('energy_6', facilityOutput.energy_6)
+            facilityOutput.energy_10 = facility_output_data.get('energy_10', facilityOutput.energy_10)
+            facilityOutput.energy_18 = facility_output_data.get('energy_18', facilityOutput.energy_18)
+            facilityOutput.energy_6FFF = facility_output_data.get('energy_6FFF', facilityOutput.energy_6FFF)
+            facilityOutput.energy_10FFF = facility_output_data.get('energy_10FFF', facilityOutput.energy_10FFF)
+            facilityOutput.save()
+        return instance
