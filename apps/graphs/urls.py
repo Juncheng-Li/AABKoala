5699d02285a3528bbf82dfd847b892abef173c65
YunningGong <81874262@qq.com>
2020-09-14 15:31:50 +0800

    fix the import bug

--- apps/graphs/urls.py
+++ apps/graphs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
-from apps.graphs import views
+from graphs import views
 
 
 # Create a router and register our viewsets with it.
