3f430afc673ecdee06f5a85d8989c7e944977f3f
geoffreychen831 <36359703+geoffreychen831@users.noreply.github.com>
2020-09-24 01:13:39 +0800

    restructure spreadsheet

--- LocalProgram/resultRequest.py
+++ LocalProgram/resultRequest.py
@@ -5,6 +5,8 @@ import mimetypes
 from pandas import json_normalize
 from LocalProgram.config import *
 from datetime import datetime
+import time
+import math
 
 class resultRequest:
 
@@ -14,13 +16,18 @@ class resultRequest:
         self.port = port
         self.conn = http.client.HTTPConnection(self.host,self.port)
 
-    def parseExcel(self) -> [str]:
+    def parseExcel(self):
         filename = "upload/uploadingData.xlsx"
         df = pd.read_excel(filename)
+        df["AuditDate"] = pd.to_datetime(df["AuditDate"], errors='coerce')
         df["AuditDate"] = df["AuditDate"].dt.strftime("%Y-%m-%d")
         resultList = []
+        ids = []
 
         for i in range(len(df)):
+            id = df.loc[i,'id']
+            if not math.isnan(id):
+                ids.append(str(int(id)))
             unested = df.loc[i,"AuditID":"Phantom"].to_json()
             facilityOutput = df.loc[i,"fac_6":"fac_10FFF"].to_json().replace("fac","energy")
             tpr = df.loc[i,"TPR_6":"TPR_10FFF"].to_json().replace("TPR","energy")
@@ -35,32 +42,59 @@ class resultRequest:
 
             resultList.append(json.dumps(result))
 
-        return resultList
+        return (resultList, ids)
 
     def insertNewResult(self):
-        resultsList = self.parseExcel()
+        resultsList = self.parseExcel()[0]
         for result in resultsList:
             payload = result
             headers = {
                 'Authorization': self.authorization,
                 'Content-Type': 'application/json'
             }
+            time1 = time.time()
             self.conn.request("POST", "/graphs/results/", payload, headers)
             res = self.conn.getresponse()
+            time2 = time.time()
             data = res.read()
-            print(data.decode("utf-8"))
+            print("insert request completed, takes time %d" %(time2-time1) + data.decode("utf-8"))
 
     def listResults(self):
         payload = ''
         headers = {
             'Authorization': self.authorization,
         }
+        print("downloading the whole dataset...")
         self.conn.request("GET", "/graphs/results/", payload, headers)
         res = self.conn.getresponse()
         data = res.read()
         content = bytes.decode(data, 'utf-8')
-        df = json_normalize(json.loads(content))
-        df.to_excel("download/" + "list" + datetime.now().strftime("%Y%m%d%H%H%S") + ".xlsx")
+        contentInJson = json.loads(content)
+        df = json_normalize(contentInJson)
+        df = df[['id', 'AuditID', 'RevisionNumber', 'FacilityName', 'FacilityID', 'Auditor1', 'Auditor2', 'Auditor3',
+                 'AuditDate', 'RepDate', 'LinacModel', 'LinacManufacturer', 'PlanningSystemManufacturer', 'tps',
+                 'Algorithm', 'kqFac', 'ACDS', 'Phantom']]
+
+
+        fac_df = json_normalize(contentInJson, record_path='facilityOutput', record_prefix='fac')
+        fac_df.columns = fac_df.columns.str.replace('energy',"")
+
+        tpr_df = json_normalize(contentInJson, record_path='TPR', record_prefix='tpr')
+        tpr_df.columns = tpr_df.columns.str.replace('energy', "")
+
+        reading_df = json_normalize(contentInJson, record_path='Reading')
+        reading_df = reading_df[['Reading_101106','Reading_110106', 'Reading_205106', 'Reading_208106',	'Reading_205206',
+                                 'Reading_208206', 'Reading_205306', 'Reading_208306', 'Reading_303106', 'Reading_305106',
+                                 'Reading_403106', 'Reading_405106', 'Reading_103110', 'Reading_110110', 'Reading_303110',
+                                 'Reading_305110', 'Reading_403110', 'Reading_405110', 'Reading_103115', 'Reading_110115',
+                                 'Reading_303115', 'Reading_305115', 'Reading_403115', 'Reading_405115', 'Reading_103118',
+                                 'Reading_110118', 'Reading_303118', 'Reading_305118', 'Reading_403118', 'Reading_405118',
+                                 'Reading_101105', 'Reading_110105', 'Reading_303105', 'Reading_305105', 'Reading_103109',
+                                 'Reading_110109', 'Reading_303109', 'Reading_305109']]
+        misdelivery_df = json_normalize(contentInJson, record_path='Misdelivery')
+
+        table = pd.concat([df, fac_df,tpr_df,reading_df, misdelivery_df], axis=1)
+        table.to_excel("download/" + "list" + ".xlsx", index=False)
         print(data.decode("utf-8"))
 
     def updateResultsWithIDs(self, resultIds):
@@ -78,6 +112,20 @@ class resultRequest:
             res = self.conn.getresponse()
             data = res.read()
             print(data.decode("utf-8"))
+
+    def updateResults(self):
+        resultsList, ids = self.parseExcel()
+        for i in range(len(ids)):
+            payload = resultsList[i]
+            headers = {
+                'Authorization': self.authorization,
+                'Content-Type': 'application/json'
+            }
+            self.conn.request("PUT", "/graphs/results/" + ids[i] + "/", payload, headers)
+            res = self.conn.getresponse()
+            data = res.read()
+            print(data.decode("utf-8"))
+            print(ids[i])
 
     def retrieveResultWithID(self, resultID):
         payload = ''
@@ -101,5 +149,3 @@ class resultRequest:
         res = self.conn.getresponse()
         data = res.read()
         print(data.decode("utf-8"))
-
-resultRequest().listResults()
