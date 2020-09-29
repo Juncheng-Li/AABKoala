eb4d36c0b28dd707885a5a82baf2bb7ada5b862b
Juncheng-Li <juncheng.alex.li@gmail.com>
2020-09-29 11:09:20 +0800

    init configuration

--- LocalProgram/config.py
+++
@@ -1,6 +0,0 @@
-# This is the configuration files for the local program
-# It can be replaced with more advanced configuration package
-
-authorization = "Basic cm9vdDpyb290"
-host = "127.0.0.1"
-port = 8000
