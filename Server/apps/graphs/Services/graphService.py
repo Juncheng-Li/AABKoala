8af7166b2b0ca0787f3f020e716521c182fc272d
YunningGong <81874262@qq.com>
2020-10-04 20:24:04 +0800

    get facilitys data

--- Server/apps/graphs/Services/graphService.py
+++ Server/apps/graphs/Services/graphService.py
@@ -70,35 +70,41 @@ def plot_NDS_IMRT(self, request):
     facilitys = json.loads(request.body.decode('utf-8')).get('facilitys')
     mode = json.loads(request.body.decode('utf-8')).get('mode')
 
-    data_format = {"c6_p11_6": [], "c6_p12_6": [], "c6_p13_6": [], "c6_p14_6": [], "c6_p15_6": [], "c6_p16_6": [],
-                   "c6_p17_6": [], "c7_p11_6": [], "c7_p12_6": [], "c7_p13_6": [], "c7_p14_6": [], "c7_p15_6": [],
-                   "c7_p16_6": [], "c7_p17_6": [], "c8_p11_6": [], "c8_p12_6": [], "c8_p13_6": [], "c8_p14_6": [],
-                   "c8_p15_6": [], "c8_p17_6": [], "c8_p18_6": [], "c6_p11_10": [], "c6_p12_10": [], "c6_p13_10": [],
-                   "c6_p14_10": [], "c6_p15_10": [], "c6_p16_10": [], "c6_p17_10": [], "c7_p11_10": [], "c7_p12_10": [],
-                   "c7_p13_10": [], "c7_p14_10": [], "c7_p15_10": [], "c7_p16_10": [], "c7_p17_10": [], "c8_p11_10": [],
-                   "c8_p12_10": [], "c8_p13_10": [], "c8_p14_10": [], "c8_p15_10": [], "c8_p17_10": [], "c8_p18_10": []}
+    data_format = ["c6_p11_6", "c6_p12_6", "c6_p13_6", "c6_p14_6", "c6_p15_6", "c6_p16_6", "c6_p17_6", "c7_p11_6",
+                   "c7_p12_6", "c7_p13_6", "c7_p14_6", "c7_p15_6", "c7_p16_6", "c7_p17_6", "c8_p11_6", "c8_p12_6",
+                   "c8_p13_6", "c8_p14_6", "c8_p15_6", "c8_p17_6", "c8_p18_6", "c6_p11_10", "c6_p12_10", "c6_p13_10",
+                   "c6_p14_10", "c6_p15_10", "c6_p16_10", "c6_p17_10", "c7_p11_10", "c7_p12_10", "c7_p13_10",
+                   "c7_p14_10", "c7_p15_10", "c7_p16_10", "c7_p17_10", "c8_p11_10", "c8_p12_10", "c8_p13_10",
+                   "c8_p14_10", "c8_p15_10", "c8_p17_10", "c8_p18_10"]
 
     all_results = Result.objects.exclude(FacilityName__in=facilitys)
     all_IMRTs = IMRT.objects.filter(result_id__in=all_results)
     all_IMRT_misdeliveries = IMRT_misdelivery.objects.filter(result_id__in=all_results)
-
     all_data = excludeMisdeliveryData(all_IMRTs, all_IMRT_misdeliveries, data_format)
     print(all_data)
+
+    facilitys_results = Result.objects.all().filter(FacilityName__in=facilitys)
+    facilitys_IMRTs = IMRT.objects.filter(result_id__in=facilitys_results)
+    facilitys_IMRT_misdeliveries = IMRT_misdelivery.objects.filter(result_id__in=facilitys_results)
+    facilitys_data = excludeMisdeliveryData(facilitys_IMRTs, facilitys_IMRT_misdeliveries, data_format)
+    print(facilitys_data)
 
     return Response(status=status.HTTP_200_OK)
 
 
 def excludeMisdeliveryData(dataset, dataset_misdeliveries, data_format):
+    data_dict = dict([(k, []) for k in data_format])
+
     for data in dataset:
         dataset_misdelivery = dataset_misdeliveries.filter(result_id=data.result_id)
         misdelivery = dataset_misdelivery[0]
 
-        for key in data_format.keys():
+        for key in data_dict.keys():
             temp = """
 if misdelivery.""" + key + """ == 1:
-    data_format[\"""" + key + """\"].append(None)
+    data_dict[\"""" + key + """\"].append(None)
 else:
-    data_format[\"""" + key + """\"].append(data.""" + key + """)
+    data_dict[\"""" + key + """\"].append(data.""" + key + """)
                 """
             exec(temp)
-    return data_format
+    return data_dict
