f1469e3d5fb11a13e9e740bcf3131b8fadf76fa4
Lin Mei-Chen <mich850901@gmail.com>
2020-10-09 18:55:04 +0800

    3DCRT and IMRT plot graph requests

--- LocalProgram/graphRequest.py
+++ LocalProgram/graphRequest.py
@@ -25,8 +25,21 @@ class graphRequest:
         filePath = "http://"+self.host+":"+str(self.port)+"/graph/"+fileName
         urllib.request.urlretrieve(filePath, localPath)
 
-    def plot_graph(self, resultIDs, mode=None):
-        payload = "{\n    \"results_list\":%s,\n    \"mode\":\"%s\"\n}" % (resultIDs, mode)
+    def plot_graph_NDS_3DCRT(self, mode, facilities):
+        payload = "{   \n    \"graphType\": \"NDS_3DCRT\",\n    \"mode\": \"%s\",\n    \"facilitys\":%s\n}" % (mode, facilities)
+        headers = {
+            'Authorization': self.authorization,
+            'Content-Type': 'application/json'
+        }
+        self.conn.request("POST", "/graphs/graphManage/", payload, headers)
+        res = self.conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+        graphInfo = json.loads(data.decode("utf-8"))
+        self.retrieve_graph(graphInfo["fileName"])
+
+    def plot_graph_NDS_IMRT(self, mode, facilities):
+        payload = "{   \n    \"graphType\": \"NDS_IMRT\",\n    \"mode\": \"%s\",\n    \"facilitys\":%s\n}" % (mode, facilities)
         headers = {
             'Authorization': self.authorization,
             'Content-Type': 'application/json'
@@ -39,7 +52,7 @@ class graphRequest:
         self.retrieve_graph(graphInfo["fileName"])
 
     def delete_graph(self, graphID):
-        payload = "{\n    \"graphs_list\":[%d]\n}" % graphID
+        payload = "{\n    \"graphs_list\":[%s]\n}" % graphID
         headers = {}
         self.conn.request("DELETE", "/graphs/graphManage/", payload, headers)
         res = self.conn.getresponse()
@@ -48,6 +61,8 @@ class graphRequest:
 
 
 #graphRequest().list_graphs()
-#graphRequest().delete_graph(36)
-graphRequest().plot_graph("[31]")
+#graphRequest().delete_graph("151")
 #graphRequest().retrieve_graph("3DCRT_1600914975424.png")
+#graphRequest().plot_graph_NDS_3DCRT("all", '{"Drever": [91], "Avocet": [99, 101]}')
+graphRequest().plot_graph_NDS_IMRT("std", '{"Drever": [91], "Avocet": [99, 101]}')
+
