26e8b7b3827b1b109727afb10564afcf918bd958
Juncheng-Li <juncheng.alex.li@gmail.com>
2020-10-14 23:27:36 +0800

    test entrypoint.sh

--- Server/docker-entrypoint.sh
+++ Server/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sleep 20
+# sleep 20
+./wait-for-it.sh db:3306 -- echo "MySQL is up"
 # python3 manage.py makemigrations graphs
 python3 manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('client', 'client@example.com', 'AA-koala123456')" | python manage.py shell
